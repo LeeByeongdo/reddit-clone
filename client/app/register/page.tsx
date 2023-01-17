943c7b82f3381bbd293796c3ab2ec6eb1c9bff2d
byeongdolee <bdbdbdlee@gmail.com>
2023-01-18 01:22:18 +0900

    ch4 auth api 연결

--- client/app/register/page.tsx
+++ client/app/register/page.tsx
@@ -1,20 +1,71 @@
+'use client';
+
+import axios from 'axios';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import { FormEvent, useState } from 'react';
+import { InputGroup } from '../../components/InputGroup';
+import { API_BASE_URL } from '../../config/constants';
 
 export default function Register() {
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState<any>();
+
+  const router = useRouter();
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+
+    try {
+      const res = await axios.post(`${API_BASE_URL}/auth/register`, {
+        email: email,
+        username,
+        password,
+      });
+
+      console.log('res', res);
+      router.push('/login');
+    } catch (e) {
+      console.log('error', e);
+    }
+  };
+
   return (
     <div className="bg-white">
       <div className="flex flex-col items-center justify-center h-screen p-6">
         <div className="w-10/12 mx-auto md:w-96">
           <h1 className="mb-2 text-lg font-medium">회원가입</h1>
-          <form>
+          <form onSubmit={handleSubmit}>
+            <InputGroup
+              placeholder="Email"
+              value={email}
+              setValue={setEmail}
+              error={errors && errors.email}
+            />
+            <InputGroup
+              placeholder="Username"
+              value={username}
+              setValue={setUsername}
+              error={errors && errors.username}
+            />
+            <InputGroup
+              placeholder="Password"
+              value={password}
+              setValue={setPassword}
+              error={errors && errors.password}
+            />
+
             <button className="w-full py-2 mb-1 text-xs font-bold text-white uppercase bg-gray-400 border border-gray-400 rounded">
-              회원 가입
+              SIGN UP
             </button>
           </form>
+
           <small>
             이미 가입하셨나요?
-            <Link className="ml-1 uppercase text-blue500" href="/login">
-              로그인
+            <Link className="ml-1 uppercase text-blue-500" href="/login">
+              <span>로그인</span>
             </Link>
           </small>
         </div>
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,22 @@
 import express from 'express';
 import morgan from 'morgan';
 import { AppDataSource } from './data-source';
+import authRouter from './routes/auth';
+import cors from 'cors';
 
 const app = express();
+const origin = 'http://localhost:3000';
 
+app.use(
+  cors({
+    origin,
+  })
+);
 app.use(express.json());
 app.use(morgan('dev'));
 app.get('/', (_, res) => res.send('running'));
+
+app.use('/api/auth', authRouter);
 
 const port = 4000;
 app.listen(port, async () => {
